fc3d5e8ceccc822a99640c6d86e6ba8338131350
Yoh-Serch <def.serch@gmail.com>
2014-12-06 13:06:41 -0600

    Primer test funcional y ahora se usa la el modulo unittest

--- functional_text.py
+++ functional_text.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title) #
+        self.fail('Finish the test!')
 
 
-assert 'Django' in browser.title
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
