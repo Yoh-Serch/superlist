d40031d35869a0f6e0f992e6263e1ec1002d6e16
Yoh-Serch <def.serch@gmail.com>
2014-12-07 01:18:24 -0600

    EL test funcional ahora confirma que el usuario ingresa un elemento TO-DO

--- functional_text.py
+++ functional_text.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,6 +16,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000') 
         # She notices the page title and header mention to-do lists 
         self.assertIn('To-Do', self.browser.title) # 
+        header_text = self.browser.find_elements_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                input_box.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+        # Simulamos que el usuario ingresa texto
+        inputbox.send_keys('Buy peacock feathers')
+        # Cuando el usuario presione enter el formulario se envia y carga información en una tabla
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
