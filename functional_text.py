f521279c73d310be76d7f2ac337404c858237b7f
Yoh-Serch <def.serch@gmail.com>
2014-12-07 00:59:29 -0600

    Vista basica que devuelve un HTML minimo

--- functional_text.py
+++ functional_text.py
@@ -12,11 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #
+        self.browser.get('http://localhost:8000') 
+        # She notices the page title and header mention to-do lists 
+        self.assertIn('To-Do', self.browser.title) # 
         self.fail('Finish the test!')
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+
