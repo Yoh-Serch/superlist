eee7b3611552a6779a71b1b32de3fae10fead2f4
Yoh-Serch <def.serch@gmail.com>
2014-12-11 14:12:53 -0600

    Se modifico el nombre del archivo functional_text.py a functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -13,22 +14,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000') 
-        # She notices the page title and header mention to-do lists 
-        self.assertIn('To-Do', self.browser.title) # 
+        self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                input_box.get_attribute('placeholder'),
-                'Enter a to-do item'
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
         # Simulamos que el usuario ingresa texto
         input_box.send_keys('Buy peacock feathers')
-        # Cuando el usuario presione enter el formulario se envia y carga información en una tabla
+        # Cuando el usuario presione enter el formulario se envia
+        # y carga información en una tabla
         input_box.send_keys(Keys.ENTER)
-        
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
@@ -40,4 +42,3 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
