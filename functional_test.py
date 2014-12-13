7f1a9e16aa809b3954def46b31fdc2de5d6b005a
= <=>
2014-12-13 01:20:37 -0600

    La vista ya recibe datos por POST y los muestra en la tabla, ademas se hizo modificacion en el test funcional ocasionando una repeticion de codigo

--- functional_test.py
+++ functional_test.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -26,18 +28,17 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
         # Simulamos que el usuario ingresa texto
-        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys('Use peacock feathers to make a fly')
         # Cuando el usuario presione enter el formulario se envia
         # y carga información en una tabla
         input_box.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item didn't appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('1: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
-
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
