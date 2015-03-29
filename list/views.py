9ff52daa82501637092f2c8f355274f3e63a0278
= <def.serch@gmail.com>
2015-03-28 19:38:56 -0600

    Redirect after POST, and show all items in template

--- list/views.py
+++ list/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from list.models import Item
 
 def home_page(request):
-
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
